60238cb2f2268eccf20a9302be8e4339e461babb
PNorfolk <34161173+PNorfolk@users.noreply.github.com>
2018-01-16 12:31:02 +0000

    Update Servosetup.py

--- Servosetup.py
+++ Servosetup.py
@@ -1,13 +1,14 @@
 ################################
-#  Setups servos 1 & 2 before  #
+#  Setups servos 1 & 3 before  #
 #      running maincode        # 
 ################################
 
 import Adafruit_BBIO.PWM as PWM
+from time import sleep
 
 # The pins we'll use
 servo1="P9_14"
-servo2="P9_16"
+servo3="P9_16"
 
 #Setting up servo 1:
 print ("Setting up servo 1: " \n "Setting duty cycle at 8.6.")
@@ -15,8 +16,11 @@ PWM.start(servo1,8.6,50)
 input("Press enter when the closed valve is in position...")
 PWM.start(servo1,10,50)
 
-#Setting up servo 2:
-print ("Setting up servo 2: " \n "Setting duty cycle at 4")
-PWM.start(servo2,4,50)
+#Setting up servo 3:
+print ("Setting up servo 3: " \n "Setting duty cycle at 4")
+PWM.start(servo3,8.6,50)
 input("Press enter when the open valve is in position...")
-PWM.start(servo2,2,50)
+PWM.start(servo3,10,50)
+sleep(1)
+PWM.start(servo3,2,50)
+
