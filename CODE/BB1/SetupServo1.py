e65ca8770f8e68073f780d1b5c8a6d717f4e3496
PNorfolk <34161173+PNorfolk@users.noreply.github.com>
2018-01-18 14:23:15 +0000

    Update SetupServo1.py

--- CODE/BB1/SetupServo1.py
+++ CODE/BB1/SetupServo1.py
@@ -1,5 +1,5 @@
 ###############################
-#      Sets up servo 1       #
+#        Sets up servos       #
 #   before running maincode   # 
 ###############################
 
@@ -8,9 +8,38 @@ from time import sleep
 
 # The pins we'll use
 servo1="P9_14"
+servo2="P9_22"
+servo3="P9_42"
 
-#Setting up servo 1:
+# Setting up servo 1: closed
 print ("Setting up servo 1: Setting duty cycle at 8.6.")
 PWM.start(servo1,8.6,50)
 raw_input("Press enter when the closed valve is in position...")
+print ("Closing valve")
 PWM.start(servo1,10,50)
+sleep(1)
+PWM.stop(servo1)
+
+# Setting up servo 2: open
+print ("Setting up servo 2: setting duty cycle at 8.6")
+PWM.start(servo2,8.6,50)
+raw_input("Press enter when the closed valve is in position...")
+print ("Closing valve")
+PWM.start(servo2,10,50)
+sleep(1)
+print ("Opening valve")
+PWM.start(servo2,2,50)
+sleep(1)
+PWM.stop(servo2)
+
+# Setting up servo 3: open
+print ("Setting up servo 3: setting duty cycle at 8.6")
+PWM.start(servo3,8.6,50)
+raw_input("Press enter when the closed valve is in position...")
+print ("Closing Valve") 
+PWM.start(servo3,10,50)
+sleep(1)
+print ("Opening valve")
+PWM.start(servo3,2,50)
+sleep(1)
+PWM.stop(servo3)
