ea9436041719ab060440890398d2d72f75977ff4
PNorfolk <34161173+PNorfolk@users.noreply.github.com>
2018-03-16 12:19:12 +0000

    Update FillRelay.py

--- Relay/FillRelay.py
+++ Relay/FillRelay.py
@@ -1,7 +1,9 @@
-import Adafruit_BBIO.GPIO as GPIO
+### Turns on the Fill Pump ###
 
+import Adafruit_BBIO.GPIO as GPIO #importing necessary module
+#Defining GPIO pins
 fill="P8_9"
 drain="P8_15"
-
+#Setting the relay to power the Fill pump
 GPIO.output(fill, GPIO.HIGH)
 GPIO.output(drain, GPIO.LOW)
