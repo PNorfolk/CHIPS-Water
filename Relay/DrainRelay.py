a9ba4b42d935af2f5aa583480ec71bc5e6345d86
PNorfolk <34161173+PNorfolk@users.noreply.github.com>
2018-03-16 12:17:46 +0000

    Update DrainRelay.py

--- Relay/DrainRelay.py
+++ Relay/DrainRelay.py
@@ -1,7 +1,10 @@
-import Adafruit_BBIO.GPIO as GPIO
+### Turns on the Drain Pump ###
 
+import Adafruit_BBIO.GPIO as GPIO #import necessary module
+
+# Defining GPIO pins 
 fill="P8_9"
 drain="P8_15"
-
+# Setting the relay to power the drain pump
 GPIO.output(fill, GPIO.LOW)
 GPIO.output(drain, GPIO.HIGH)
